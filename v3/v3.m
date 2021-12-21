effe48ee6e5b71c64921b9e40378b7c938d32fc8
Jeffrey Luci <88209977+jeffreyluci@users.noreply.github.com>
2021-12-21 14:30:25 -0500

    Update v3.m
    Added NifTI compatibility and PDF view exporting. Numerous code modernizations as well.

--- v3/v3.m
+++ v3/v3.m
@@ -1,47 +1,56 @@
 function v3(IM)
 
 if isdeployed || ~exist('IM', 'var')
-    dirname = uigetdir('.', 'Select DICOM Directory . . .');
-    listing = dir(dirname);
-    imageNum=1;
-    waitfig=waitbar(0, 'Reading DICOM headers . . .');
-    for ii = 3:numel(listing)
-        if(strcmp('mcd', strtok(fliplr(listing(ii).name), '.')))
-            if(~exist('sliceOrder', 'var'))
-                sliceOrder=[];
+    %dirname = uigetdir('.', 'Select image directory . . .');
+    [filename, pathname, ~] = uigetfile({'*.dcm; *.ima; *.nii', 'DICOM and NIfTI files'}, 'Pick 3D image file');
+    listing = dir(pathname);
+
+    if(isdicom(fullfile(pathname, filename)))
+        imageNum=1;
+        sliceOrder=[];
+        fileList = {};
+        waitfig=waitbar(0, 'Reading DICOM headers . . .');
+        for ii = 3:numel(listing)
+            curfile = fullfile(pathname, listing(ii).name);
+            if(isdicom(curfile))
+                fileList{numel(fileList)+1} = curfile;
+                hdr=dicominfo(curfile);
+                sliceOrder = [sliceOrder hdr.SliceLocation];
             end
-            hdr=dicominfo([dirname '/' listing(ii).name]);
-            sliceOrder = [sliceOrder hdr.SliceLocation];
+            waitbar(ii/numel(listing), waitfig);
         end
-        waitbar(ii/numel(listing), waitfig);
+        close(waitfig);
+        sortedSliceOrder = sort(sliceOrder);
+        IM=int16(zeros(hdr.Height, hdr.Width, numel(sliceOrder)));
+        waitfig=waitbar(0, 'Importing DICOMs . . .');
+        for ii = 1:numel(sliceOrder)
+            IM(:,:,ii)=dicomread(fileList{find(sortedSliceOrder(ii)==sliceOrder)});
+            waitbar(ii/numel(sliceOrder), waitfig);
+        end
+        close(waitfig);
+    elseif(isnifti(fullfile(pathname, filename)))
+        IM = niftiread(fullfile(pathname, filename));
+    else
+        error('Filetype not supported.');
     end
-    close(waitfig);
-    sortedSliceOrder = sort(sliceOrder);
-    IM=int16(zeros(hdr.Height, hdr.Width, numel(sliceOrder)));
-    waitfig=waitbar(0, 'Importing DICOMs . . .');
-    for ii = 1:numel(sliceOrder)
-        name=sprintf('%s/I%04d.dcm', dirname, find(sortedSliceOrder(ii)==sliceOrder));
-        IM(:,:,ii)=dicomread(name);
-        waitbar(ii/numel(sliceOrder), waitfig);
-    end
-    close(waitfig);
 end
 
-maxInt=max(IM(:));
-minInt=min(IM(:));
+maxInt=max(IM, [], 'all');
+minInt=min(IM, [], 'all');
 
 rotateUndo =  [0 0 0];
 rotateReset = [0 0 0];
 sliceUndo = round(size(IM)/2);
 method='crop';
+viewList = [0 0 0];
 
 fig = figure;
 setappdata(fig, 'visCh', 'on');
 pos=get(fig, 'Position');
 
 set(fig,'Position',    [pos(1) pos(2) 1000 460], ...
-        'Name',        ['View3d ' char(169) ' 2011 Jeffrey Luci, ' ...
-        'The University of Texas at Austin'], ...
+        'Name',        ['View3d ' char(169) ' 2011-2021 Jeffrey Luci, ' ...
+        'Rutgers University'], ...
         'ToolBar',     'none', ...
         'MenuBar',     'none', ...
         'NumberTitle', 'off', ...
@@ -85,14 +94,14 @@ SCH = uicontrol(fig,            'Style',           'checkbox', ...
                                 'Value',           0, ...
                                 'String',          'Hide Crosshairs', ...
                                 'Position',        [160 10 130 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'Callback',        @setCrosshairs);
               
 cropRotate = uicontrol(fig,     'Style',           'checkbox', ...
                                 'Value',           1, ...
                                 'String',          'Crop Rotated Volume', ...
                                 'Position',        [20 30 130 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8]);
+                                'BackgroundColor', [0.9400 0.9400 0.9400]);
                      
 rotateResetButton=uicontrol(fig,'Style',           'pushbutton', ....
                                 'String',          'Reset Rotations', ...
@@ -102,71 +111,88 @@ rotateResetButton=uicontrol(fig,'Style',           'pushbutton', ....
 
 rotateA1EditBox = uicontrol(fig,'Style',           'Edit', ...
                                 'String',          '0', ...
-                                'Position',        [140 60 40 20], ...
+                                'Position',        [100 60 40 20], ...
                                 'Callback',        @rotateA1);
                    
 rotateA2EditBox = uicontrol(fig,'Style',           'Edit', ...
                                 'String',          '0', ...
-                                'Position',        [480 60 40 20],...
+                                'Position',        [440 60 40 20],...
                                 'Callback',        @rotateA2);
                    
 rotateA3EditBox = uicontrol(fig,'Style',           'Edit', ...
                                 'String',          '0', ...
-                                'Position',        [820 60 40 20],...
+                                'Position',        [780 60 40 20],...
                                 'Callback',        @rotateA3);
-                            
+
+
+
 slice1EditBox = uicontrol(fig,  'Style',           'Edit', ...
                                 'String',          num2str(round(size(IM,3)/2)),...
-                                'Position',        [98 60 40 20], ...
+                                'Position',        [58 60 40 20], ...
                                 'Callback',        @specifySlice1);
 
 slice2EditBox = uicontrol(fig,  'Style',           'Edit', ...
                                 'String',          num2str(round(size(IM,2)/2)),...
-                                'Position',        [438 60 40 20], ...
+                                'Position',        [398 60 40 20], ...
                                 'Callback',        @specifySlice2);
                             
 slice3EditBox = uicontrol(fig,  'Style',           'Edit', ...
                                 'String',          num2str(round(size(IM,1)/2)),...
-                                'Position',        [778 60 40 20], ...
+                                'Position',        [738 60 40 20], ...
                                 'Callback',        @specifySlice3);
                             
 slice1Label = uicontrol(fig,    'Style',           'Text', ...
-                                'Position',        [60 58 35 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'Position',        [20 58 35 20], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'FontSize',        9, ...
                                 'String',          'Slice:');
                             
 slice2Label = uicontrol(fig,    'Style',           'Text', ...
-                                'Position',        [400 58 35 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
-                                'FontSize',        9, ...
+                                'Position',        [360 58 35 20], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
+                                'FontSize',        9, ....
                                 'String',          'Slice:');
                             
 slice3Label = uicontrol(fig,    'Style',           'Text', ...
-                                'Position',        [740 58 35 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'Position',        [700 58 35 20], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'FontSize',        9, ...
                                 'String',          'Slice:'); 
                    
 rotate1Label = uicontrol(fig,   'Style',           'Text', ...
-                                'Position',        [180 60 7 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'Position',        [140 60 7 20], ...
                                 'FontSize',        11, ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'String',          char(176));
                          
 rotate2Label = uicontrol(fig,   'Style',           'Text', ...
-                                'Position',        [520 60 7 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'Position',        [480 60 7 20], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'FontSize',        11, ...
                                 'String',          char(176)); 
                          
 rotate3Label = uicontrol(fig,   'Style',           'Text', ...
-                                'Position',        [860 60 7 20], ...
+                                'Position',        [820 60 7 20], ...
                                 'BackgroundColor', [0.8 0.8 0.8], ...
                                 'FontSize',        11, ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'String',          char(176));
 
-              
+reorient1Button =uicontrol(fig,'Style',           'pushbutton', ...
+                                'String',          'Reorient View', ...
+                                'Position',        [165 60 100 20], ...
+                                'Callback',        @reorient1);
+
+reorient2Button =uicontrol(fig,'Style',           'pushbutton', ...
+                                'String',          'Reorient View', ...
+                                'Position',        [505 60 100 20], ...
+                                'Callback',        @reorient2);
+
+reorient3Button =uicontrol(fig,'Style',           'pushbutton', ...
+                                'String',          'Reorient View', ...
+                                'Position',        [845 60 100 20], ...
+                                'Callback',        @reorient3);
+            
 maxSlider = uicontrol(fig,      'Style',           'Slider', ...
                                 'Position',        [300 30 200 20], ...
                                 'Max',             max(IM(:))*1.01, ...
@@ -183,17 +209,22 @@ minSlider = uicontrol(fig,      'Style',           'Slider', ...
                     
 maxSliderLabel = uicontrol(fig, 'Style',           'Text', ...
                                 'Position',        [502 26 130 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'String',          'Maximum Intensity');
                          
 minSliderLabel = uicontrol(fig, 'Style',           'Text', ...
                                 'Position',        [500 6 130 20], ...
-                                'BackgroundColor', [0.8 0.8 0.8], ...
+                                'BackgroundColor', [0.9400 0.9400 0.9400], ...
                                 'String',          'Minimum Intensity');   
+
+exportPDFButton =uicontrol(fig,'Style',           'pushbutton', ...
+                                'String',          'Export PDF Views', ...
+                                'Position',        [635 10 175 25], ...
+                                'Callback',        @exportPDF);
                          
 exportDataButton =uicontrol(fig,'Style',           'pushbutton', ...
                                 'String',          'Export Volume to Workspace', ...
-                                'Position',        [800 10 175 25], ...
+                                'Position',        [810 10 175 25], ...
                                 'Callback',        @exportData);
                             
 if isdeployed
@@ -212,6 +243,9 @@ end
         imagesc(squeeze(IM(round(pos1(2)),:,:)), ...
             'Parent', axis3, [minInt maxInt]);
         axis(axis3, 'off', 'image'),colormap gray;
+        set(axis1, 'View', [viewList(1), 90]);
+        set(axis2, 'View', [viewList(2), 90]);
+        set(axis3, 'View', [viewList(3), 90]);
         set(slice2EditBox, 'String', num2str(round(pos1(1))));
         set(slice3EditBox, 'String', num2str(round(pos1(2))));
         P3=impoint(axis3, pos3(1), pos1(1));
@@ -230,6 +264,9 @@ end
         imagesc(squeeze(IM(round(pos2(2)),:,:)), ...
             'Parent', axis3, [minInt maxInt]);
         axis(axis3, 'off', 'image'),colormap gray;
+        set(axis2, 'View', [viewList(2), 90]);
+        set(axis1, 'View', [viewList(1), 90]);
+        set(axis3, 'View', [viewList(3), 90]);
         set(slice1EditBox, 'String', num2str(round(pos2(1))));
         set(slice3EditBox, 'String', num2str(round(pos1(2))));
         P3=impoint(axis3, pos2(1), pos3(2));
@@ -248,6 +285,9 @@ end
         imagesc(squeeze(IM(:,round(pos3(2)),:)), ...
             'Parent', axis2, [minInt maxInt]);
         axis(axis2, 'off', 'image'),colormap gray;
+        set(axis3, 'View', [viewList(3), 90]);
+        set(axis1, 'View', [viewList(1), 90]);
+        set(axis2, 'View', [viewList(2), 90]);
         set(slice1EditBox, 'String', num2str(round(pos2(1))));
         set(slice2EditBox, 'String', num2str(round(pos1(1))));
         P2=impoint(axis2, pos3(1), pos2(2));
@@ -277,6 +317,7 @@ end
             maxInt=newVal;
             updatePanel1(getPosition(P1));
             updatePanel2(getPosition(P2));
+            updatePanel3(getPosition(P3));
         end
     end
 
@@ -290,6 +331,7 @@ end
             minInt=newVal;
             updatePanel1(getPosition(P1));
             updatePanel2(getPosition(P2));
+            updatePanel3(getPosition(P3));
         end
     end
 
@@ -316,6 +358,7 @@ end
             IM=temp;
             updatePanel1(getPosition(P1));
             updatePanel2(getPosition(P2));
+            updatePanel3(getPosition(P3));
             clear temp;
             rotateReset(1) = rotateReset(1) + newVal;
             set(rotateA1EditBox, 'String', num2str(rotateReset(1)));
@@ -346,6 +389,7 @@ end
             IM=ipermute(temp, [1 3 2]);
             updatePanel1(getPosition(P1));
             updatePanel2(getPosition(P2));
+            updatePanel3(getPosition(P3));
             clear temp;
             rotateReset(2) = rotateReset(2) + newVal;
             set(rotateA2EditBox, 'String', num2str(rotateReset(2)));
@@ -376,6 +420,7 @@ end
             IM=ipermute(temp, [3 2 1]);
             updatePanel1(getPosition(P1));
             updatePanel2(getPosition(P2));
+            updatePanel3(getPosition(P3));
             clear temp;
             rotateReset(3) = rotateReset(3) + newVal;
             set(rotateA3EditBox, 'String', num2str(rotateReset(3)));
@@ -493,6 +538,100 @@ end
             sliceUndo(1)=newVal;
             updatePanel3(pos3);
         end
+    end
+
+    function reorient1(~, ~)
+        switch viewList(1)
+            case 0
+                viewList(1) = 90;
+            case 90
+                viewList(1) = 180;
+            case 180
+                viewList(1) = 270;
+            case 270
+                viewList(1) = 0;
+        end
+        updatePanel1(getPosition(P1));
+        updatePanel2(getPosition(P2));
+        updatePanel3(getPosition(P3));
+    end
+
+    function reorient2(~, ~)
+        switch viewList(2)
+            case 0
+                viewList(2) = 90;
+            case 90
+                viewList(2) = 180;
+            case 180
+                viewList(2) = 270;
+            case 270
+                viewList(2) = 0;
+        end
+        updatePanel1(getPosition(P1));
+        updatePanel2(getPosition(P2));
+        updatePanel3(getPosition(P3));
+    end
+
+    function reorient3(~, ~)
+        switch viewList(3)
+            case 0
+                viewList(3) = 90;
+            case 90
+                viewList(3) = 180;
+            case 180
+                viewList(3) = 270;
+            case 270
+                viewList(3) = 0;
+        end
+        updatePanel1(getPosition(P1));
+        updatePanel2(getPosition(P2));
+        updatePanel3(getPosition(P3));
+    end
+
+    function exportPDF(~,~)
+
+        pdfTitle = inputdlg('Enter PDF Title. <Enter> for none.');
+        
+        pdfFig = figure('PaperType', 'usletter', ...
+                        'PaperUnits', 'inches', ...
+                         'PaperSize', [8.5 11], ...
+                         'Position', [0 100 800 1050], ...
+                         'Visible', 'off');
+
+        pdfAxis1 = subplot(2,2,1);
+        pos1=getPosition(P1);
+        pos2=getPosition(P2);
+        pos3=getPosition(P3);
+        
+        imagesc(IM(:,:,round(pos2(1))), ...
+            'Parent', pdfAxis1, [minInt maxInt]);
+        axis(pdfAxis1, 'off', 'image'),colormap gray;
+        set(pdfAxis1, 'View', [viewList(1), 90]);
+
+        pdfAxis2 = subplot(2,2,2);
+        imagesc(squeeze(IM(:,round(pos3(2)),:)), ...
+            'Parent', pdfAxis2, [minInt maxInt]);
+        axis(pdfAxis2, 'off', 'image'),colormap gray;
+        set(pdfAxis2, 'View', [viewList(2), 90]);
+
+        pdfAxis3 = subplot(2,2,3);
+        imagesc(squeeze(IM(round(pos1(2)),:,:)), ...
+            'Parent', pdfAxis3, [minInt maxInt]);
+        axis(pdfAxis3, 'off', 'image'),colormap gray;
+        set(pdfAxis3, 'View', [viewList(3), 90]);
+
+        set(pdfAxis3, 'Position', [0.25 0.025 0.425 0.425])
+        set(pdfAxis2, 'Position', [0.55 0.45 0.425 0.425])
+        set(pdfAxis1, 'Position', [0.025 0.45 0.425 0.425])
+
+        if ~cellfun(@isempty, pdfTitle)
+            sgtitle(pdfTitle, 'FontSize', 18)
+        end
+
+        print(pdfFig, '-dpdf', 'v3 printout.pdf')
+        close(pdfFig);
+        clear('pdfFig');
+
     end
 
 end
