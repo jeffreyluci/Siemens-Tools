29cdde179a2e45c0b14939ac32461a5cf5de7951
Jeffrey Luci <88209977+jeffreyluci@users.noreply.github.com>
2023-02-20 13:47:03 -0500

    Update parseMrProt.m

--- parseMrProt/parseMrProt.m
+++ parseMrProt/parseMrProt.m
@@ -33,7 +33,7 @@ function [mrProt, zeroList] = parseMrProt(inputArg)
 if ischar(inputArg)
     if contains(inputArg, 'ASCCONV')
         tagFullText = inputArg;
-    elseif isdicom(inputArg)
+    elseif isdicom(inputArg, 'file')
         hdr = dicominfo(inputArg);
     end
 else
