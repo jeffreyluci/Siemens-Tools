9eeeaa9a7ddea492d0899551641c4ead5bf16fba
Jeffrey Luci <88209977+jeffreyluci@users.noreply.github.com>
2023-06-14 08:06:18 -0400

    Update maskPhasemap.m
    code consistency improvements, switched from 3D ROI to 2D filling for better results, NIfTI header description notation for provenance, default thresh 0.09->0.1, and help text additions.

--- maskPhasemap/maskPhasemap.m
+++ maskPhasemap/maskPhasemap.m
@@ -4,16 +4,24 @@ function maskPhasemap(magImageFile, phaseImageFile, thresholdScale)
 %               the phase image using a threshold value. The output is a
 %               NIfTI file of the phase difference image that is compressed
 %               or not, depending on the status of the input NIfTI file.
+%               The output file will be named the same as the phase NIfTI
+%               file, with "_masked" appended.
 %
 %Usage: maskPhasemap(magnitudeNifti, phaseNifti, <threshold>)
 %
 %       magnitudeNifti = location of the NIfTI with the magnitude images
 %       phaseNifti = location of the NIfTI with the phase difference images
-%       threshold = fraction of the maximum magnitudepixel value to use for
-%                   the mask threshold. Optional. Default = 0.09
+%       threshold = fraction of the maximum magnitude pixel value to use
+%                   for the mask threshold. Optional. Default = 0.1
 
 %Author: Jeffrey Luci, jeffrey.luci@rutgers.edu
 %Version: 20230613 - initial release
+%Version: 20230614 - code consistency improvements, switched from 3D ROI to
+%                    2D filling for better results, NIfTI header description
+%                    notation for provenance, default thresh 0.09->0.1,
+%                    and help text additions.
+
+version = '20230614';
 
 %Check to make sure the NIfTI files are readable
 if ~exist(magImageFile, 'file')
@@ -23,19 +31,21 @@ if ~exist(phaseImageFile, 'file')
     error('The phase volume is not found.');
 end
 
-%Check to see if the files are compressed 
+%Check to see if the phase file is compressed, set flag appropriately 
 [outputFilePath, outputFileBase, outputFileExtension] = fileparts(phaseImageFile);
 if strcmp(outputFileExtension, '.gz')
-    outputFileBase = strcat(outputFileBase(1:length(outputFileBase)-4), '_masked');
+    [~, outputFileBase, ~] = fileparts(outputFileBase);
     compressedFlag = true;
 else
-    outputFileBase = strcat(outputFileBase, '_masked');
     compressedFlag = false;
 end
 
+%Append the output file name to designate the change
+outputFileBase = strcat(outputFileBase, '_masked');
+
 %Set the threshold value if the user did not specify as a function argument
 if ~exist('thresholdScale', 'var')
-    thresholdScale = 0.09;
+    thresholdScale = 0.1;
 end
 
 %read in the images and the phase difference header
@@ -43,16 +53,26 @@ magImage    = niftiread(magImageFile);
 phaseImage  = niftiread(phaseImageFile);
 phaseHeader = niftiinfo(phaseImageFile);
 
-%Apply the threashold to the phase difference images
+%Note the processing step in the NIfTI header description
+if isfield(phaseHeader, 'Description')
+    phaseHeader.Description = strcat(phaseHeader.Description, ...
+                              ';Masked using maskPhasemap.m version: ', ...
+                              version);
+else
+    phaseHeader.Description = strcat('Masked using maskPhasemap.m version: ', ...
+                              version);
+end
+
+%Apply the threshold to the phase difference images
 threshold = thresholdScale*max(magImage, [], 'all');
 mask = magImage > threshold;
-mask = imfill(mask, 'holes');
+for ii = 1:size(mask, 3)
+    mask(:,:,ii) = imfill(mask(:,:,ii), 'holes');
+end
 phaseImage(~mask) = 0;
 
 %write the new NIfTI recycling the phase difference header on the new file
-niftiwrite(phaseImage, strcat(outputFilePath, filesep, outputFileBase), ...
+niftiwrite(phaseImage, fullfile(outputFilePath, outputFileBase), ...
            phaseHeader, "Compressed", compressedFlag);
-
-
 
 end
